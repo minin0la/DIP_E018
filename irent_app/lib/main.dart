4040db91a442c377c4aaed2a78212dba3ca7ec26
sailesh.gopalakrishnan <sailsg111@gmail.com>
2022-01-26 15:32:30 +0800

    added some changes to create checkbox

--- irent_app/lib/main.dart
+++ irent_app/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
