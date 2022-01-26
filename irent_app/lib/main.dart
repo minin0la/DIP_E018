b7d6fb65f118fcf205c488f02cc3c2261d09e05d
Natawat Hebnak <minin0la17@gmail.com>
2022-01-26 14:55:19 +0800

    #3 Added firebase to the project

--- irent_app/lib/main.dart
+++ irent_app/lib/main.dart
@@ -1,8 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
