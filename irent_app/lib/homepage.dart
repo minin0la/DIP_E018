242459df1ac22e6d479420cd7d39aaea2def6bc1
sailesh.gopalakrishnan <sailsg111@gmail.com>
2022-02-04 22:24:45 +0800

    positioned the CircleAvatar and Name text in the Drawer widget

--- irent_app/lib/homepage.dart
+++ irent_app/lib/homepage.dart
@@ -16,62 +16,107 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        drawer: Drawer(
-            child: ListView(
-          padding: EdgeInsets.zero,
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const DrawerHeader(
-              child: Text('yes'),
-            ),
-            ListTile(
-              title: const Text('Home'),
-              leading: Icon(Icons.home),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ListTile(
-              title: const Text('Account'),
-              leading: Icon(Icons.person),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ListTile(
-              title: const Text('History'),
-              leading: Icon(Icons.calendar_view_day_rounded),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ListTile(
-              title: const Text('Feedback'),
-              leading: Icon(Icons.feedback),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginRegisterScreen()));
+        drawer: Theme(
+          data: Theme.of(context).copyWith(canvasColor: Colors.grey),
+          child: Drawer(
+              child: ListView(
+            padding: EdgeInsets.zero,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(left: 30, top: 30),
+                child: Container(
+                  height: 200,
+                  child: DrawerHeader(
+                    child: Column(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        CircleAvatar(
+                          radius: 40,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Stephanie',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                title: const Text('Home'),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  Navigator.pop(context);
                 },
-                child: Text('Submit')),
-          ],
-        )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                title: const Text('Account'),
+                leading: Icon(Icons.person),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                title: const Text('History'),
+                leading: Icon(Icons.calendar_view_day_rounded),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                title: const Text('Feedback'),
+                leading: Icon(Icons.feedback),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Container(
+                padding: EdgeInsets.only(right: 90, left: 20),
+                margin: EdgeInsets.only(top: 120),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.grey.shade300,
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const LoginRegisterScreen()));
+                    },
+                    child: Text('Sign Out')),
+              )
+            ],
+          )),
+        ),
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.black),
