1a2e27fbb29000ffde4ef1b857a13624d63590a2
michelleandrea28 <mich0090@e.ntu.edu.sg>
2022-03-15 23:44:21 +0800

    update main.dart

--- irent_app/lib/main.dart
+++ irent_app/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         primaryColor: Color(0xFFFBFBFF),
       ),
-      home: AdminSwitchNavBar(),
+      home: MainPage(),
     );
   }
 }
