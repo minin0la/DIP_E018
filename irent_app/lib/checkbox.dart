71eaf93e33f40f3c3997793c0a052867db07fe2d
xincixx <xincixx@gmail.com>
2022-03-16 04:29:53 +0800

    Create checkbox.dart

---
+++ irent_app/lib/checkbox.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class checkbox extends StatefulWidget {
+  const checkbox({Key? key}) : super(key: key);
+
+  @override
+  State<checkbox> createState() => _checkboxState();
+}
+
+class _checkboxState extends State<checkbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 30,
+          ),
+          Checkbox(
+            checkColor: Colors.white,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
