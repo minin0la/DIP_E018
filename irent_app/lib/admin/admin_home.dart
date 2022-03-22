7b0c8525f5e5989ae629bfbc1f2e6e4293519371
michelleandrea28 <mich0090@e.ntu.edu.sg>
2022-03-22 18:00:49 +0800

    Feature-admin_success_transactions

--- irent_app/lib/admin/admin_home.dart
+++ irent_app/lib/admin/admin_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:irent_app/user_store_uroc.dart';
+import 'package:irent_app/admin/admin_add_store.dart';
 import 'dart:ui';
 import '../app_icons.dart';
 
@@ -11,9 +12,32 @@ class admin_home extends StatefulWidget {
 }
 
 class _admin_homeState extends State<admin_home> {
+  final Color marigold = const Color(0xFFECA400);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              backgroundColor: marigold,
+              heroTag: 'add_store',
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddStorePage()));
+              },
+              child: Icon(
+                Icons.add,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
           Container(
@@ -104,7 +128,7 @@ class _admin_homeState extends State<admin_home> {
                   ],
                   borderRadius: BorderRadius.circular(10),
                   image: new DecorationImage(
-                    image: AssetImage('images/Image_uroc.png'),
+                    image: AssetImage('images/uroc.jpeg'),
                     colorFilter: new ColorFilter.mode(
                         Color.fromRGBO(0, 29, 74, 0.6000000238418579),
                         BlendMode.hardLight),
--- irent_app/lib/admin/admin_transactions.dart
+++ irent_app/lib/admin/admin_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:irent_app/admin/admin_constants.dart';
 import 'package:irent_app/user_store_uroc.dart';
 import 'dart:ui';
 import '../app_icons.dart';
@@ -11,8 +12,272 @@ class admin_transactions extends StatefulWidget {
 }
 
 class _admin_transactionsState extends State<admin_transactions> {
+  final Color white = const Color(0xFFFBFBFF);
+  final Color oxford = const Color(0xFF001D4A);
+  final Color indigo = const Color(0xFF27476E);
+  final Color aliceblue = const Color(0xFF81A4CD);
+  final Color iceberg = const Color(0xFFDBE4EE);
+  final Color marigold = const Color(0xFFECA400);
+  final Color transparent = const Color(0x4DE3E3E3);
+
+  final TextStyle titleStyle = const TextStyle(
+      fontFamily: 'SF_Pro_Rounded',
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: Color(0xFF001D4A));
+
+  final TextStyle fieldStyle = const TextStyle(
+      fontFamily: 'SF_Pro_Rounded',
+      fontSize: 15,
+      fontWeight: FontWeight.w400,
+      color: Color(0xFF001D4A));
+  final TextStyle subtitleStyle = const TextStyle(
+      fontFamily: 'SF_Pro_Rounded',
+      fontWeight: FontWeight.w600,
+      fontSize: 20,
+      color: Color(0xFF001D4A));
+  final TextStyle moneyStyle = const TextStyle(
+      fontFamily: 'SF_Pro_Rounded',
+      fontWeight: FontWeight.w600,
+      fontSize: 30,
+      color: Color(0xFFFBFBFF));
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Transactions'));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Earnings',
+                      style: subtitleStyle,
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Container(
+                          height: 50,
+                          width: 130,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: aliceblue),
+                          child: Center(
+                            child: Text(
+                              '\$20',
+                              style: moneyStyle,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'My Bank Account',
+                      style: subtitleStyle,
+                    ),
+                    Divider(
+                      color: iceberg,
+                      thickness: 2,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: 40,
+                        width: 190,
+                        child: Row(
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Icon(
+                                  AppIcons.addbankaccount,
+                                  color: indigo,
+                                )),
+                            Expanded(
+                                flex: 3,
+                                child: Text('Add Bank Account',
+                                    style: TextStyle(
+                                        fontFamily: 'SF_Pro_Rounded',
+                                        fontWeight: FontWeight.w300,
+                                        fontSize: 16,
+                                        color: oxford)))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: iceberg,
+                      thickness: 2,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 4,
+                          child: Text(
+                            'Recent Transactions',
+                            style: subtitleStyle,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: _dropDown(
+                              context: context,
+                              items: ['Today', 'Past 7 Days', 'Past Month']),
+                        )
+                      ],
+                    ),
+                    Divider(color: iceberg, thickness: 2),
+                  ]),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: ListView.builder(
+                    itemCount: transactionData.length,
+                    itemBuilder: (context, index) {
+                      for (var trans in transactionData) {
+                        return _transaction(
+                            context: context,
+                            ticketNumber: transactionData[index]['ticketNumber']
+                                .toString(),
+                            paymentDate: transactionData[index]['paymentDate']
+                                .toString(),
+                            totalAmount: transactionData[index]['totalAmount']
+                                .toString());
+                      }
+                      throw 'No Data Found';
+                    }),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _dropDown({
+    required BuildContext context,
+    required List<String> items,
+  }) {
+    var dropdownValue;
+    return Padding(
+      padding: const EdgeInsets.only(top: 15),
+      child: Container(
+        height: 30,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: iceberg,
+        ),
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton<String>(
+            isExpanded: true,
+            icon: Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: const Icon(Icons.keyboard_arrow_down_sharp),
+            ),
+            iconSize: 28,
+            iconEnabledColor: Color(0xFF001D4A),
+            elevation: 16,
+            style: fieldStyle,
+            hint: Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text('Select', style: fieldStyle),
+            ),
+            value: dropdownValue,
+            isDense: true,
+            onChanged: (newValue) {
+              setState(() {
+                dropdownValue = newValue;
+              });
+            },
+            items: items.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _transaction(
+      {required BuildContext context,
+      required String ticketNumber,
+      required String paymentDate,
+      required String totalAmount}) {
+    TextStyle _textStyle({required double size, required FontWeight weight}) {
+      return TextStyle(
+          fontFamily: 'SF_Pro_Rounded',
+          fontSize: size,
+          fontWeight: weight,
+          color: Color(0xFF001D4A));
+    }
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(flex: 1, child: Icon(AppIcons.receivepayment, size: 25)),
+            Expanded(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Income',
+                    style: _textStyle(size: 15, weight: FontWeight.w600),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'from',
+                        style: _textStyle(size: 10, weight: FontWeight.w200),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'Booking #$ticketNumber',
+                        style: _textStyle(size: 10, weight: FontWeight.w400),
+                      )
+                    ],
+                  ),
+                  Text(
+                    paymentDate,
+                    style: _textStyle(size: 12, weight: FontWeight.w500),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Text(
+                    '\$$totalAmount',
+                    style: _textStyle(size: 15, weight: FontWeight.w500),
+                  ),
+                  Text(
+                    'Completed',
+                    style: _textStyle(size: 10, weight: FontWeight.w300),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        Divider(color: iceberg, thickness: 1)
+      ],
+    );
   }
 }
