6478fc144535e2da314868570173c1de77ea85d6
Ding Wei <dwlyz123@gmail.com>
2022-04-03 19:31:18 +0800

    added firebase code

---
+++ hardware/firebase.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+class fb:
+
+    def init():
+        cred = credentials.Certificate("./key/dip-ee018-firebase-adminsdk-b6phy-7152e25998.json")
+        firebase_admin.initialize_app(cred)
+
+    def updateuserfb():
+        db = firestore.client()
+        with open('./authentication/userencrypt.txt', 'r') as f:
+            encrypted = f.read()
+        db.collection("Key").document("user").update({"value": str(encrypted)})
+
+    def updateadminfb():
+        db = firestore.client()
+        with open('./authentication/adminencrypt.txt', 'r') as f:
+            encrypted = f.read()
+        db.collection("Key").document("admin").update({"value": str(encrypted)})
+
+    def updatedata(store, box,field,value):
+        db = firestore.client()
+        db.collection(str(store)).document(str(box)).update({str(field): str(value)})
